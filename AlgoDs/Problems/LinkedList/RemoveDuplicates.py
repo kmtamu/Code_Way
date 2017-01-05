1e90a509f2b29586509467d1a655a4349281e3bd
kmtamu <kmr.manish@tamu.edu>
2017-01-05 09:05:09 -0600

    Updated

--- AlgoDs/Problems/LinkedList/RemoveDuplicates.py
+++ AlgoDs/Problems/LinkedList/RemoveDuplicates.py
@@ -3,9 +3,15 @@ from Problems.LinkedList.Node import Node
 
 
 def RemoveDuplicate(myll):
-    tmp=myll.head
-    tmp2=tmp.nextNode
-    if tmp.data == tmp2.data:
+	tmp = myll.head
+	tmp2 = myll.head.nextNode
+	while tmp2!=None:
+		if tmp.data == tmp2.data:
+			tmp.nextNode = tmp2.nextNode
+			tmp2 = tmp2.nextNode
+		else:
+			tmp=tmp.nextNode
+			tmp2=tmp2.nextNode
         
 
 myll = LinkedList()
